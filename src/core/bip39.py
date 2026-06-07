a5c62c7532143281e55c1bdf7a68c8c6b94e5791
Krunixbase <132249302+krunixbase-dev@users.noreply.github.com>
2026-06-07 21:25:01 +0200

    BIP39 class with mnemonic conversion methods
    Added BIP39 class with placeholder methods for mnemonic conversion.

---
+++ src/core/bip39.py
@@ -0,0 +1,8 @@
+class BIP39:
+    def mnemonic_to_entropy(self, mnemonic: str) -> bytes:
+        # TODO: implement deterministic conversion
+        raise NotImplementedError
+
+    def entropy_to_mnemonic(self, entropy: bytes) -> str:
+        # TODO: implement deterministic conversion
+        raise NotImplementedError
